605d8e9e87cf9c942aacf5914cf827015241c7aa
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-12-16 19:03:18 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+bool funzione(char a[10][20], char b[20], int &pos) {
 
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 20; j++) {
+            if (b[j] != a[i][j]) {
+                j = 20;
+            } else if(j==19){
+                pos = i;
+                return true;
+            }
+        }
+    }
+    return false;
+}
