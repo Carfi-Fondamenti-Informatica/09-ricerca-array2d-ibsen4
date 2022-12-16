4a768b553c4293169a2409f2dbe8cc25e4eb663d
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-12-16 19:04:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    char a[10][20];
+    char b[20];
+    int pos = 0;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 20; j++) {
+            a[i][j] = ' ';
+            b[j] = ' ';
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+    cin >> b;
+
+
+    if (funzione(a, b, pos)) {
+        cout << pos;
+    } else { cout << "non presente"; }
+
+    return 0;
 }
